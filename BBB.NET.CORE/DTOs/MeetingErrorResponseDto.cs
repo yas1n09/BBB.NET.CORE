709ede9985f5b2a92047458bb0da01987aa0fb8e
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-12-30 17:31:23 +0300

    -

---
+++ BBB.NET.CORE/DTOs/MeetingErrorResponseDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BBB.NET.CORE.DTOs
+{
+    [XmlRoot("response")]
+    public class MeetingErrorResponseDto
+    {
+        [XmlElement("returncode")]
+        public string ReturnCode { get; set; } = "FAILED";
+
+        [XmlElement("message")]
+        public string Message { get; set; }
+
+        [XmlElement("details")]
+        public string Details { get; set; }
+
+        public MeetingErrorResponseDto() { }
+
+        public MeetingErrorResponseDto(string message, string details)
+        {
+            Message = message;
+            Details = details;
+        }
+    }
+}
+
